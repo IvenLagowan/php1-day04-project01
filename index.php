65a949c91d0891c4d392a4a7fc866a87e017fc32
Iven DF <94290469+ivendf@users.noreply.github.com>
2022-06-02 03:05:28 -0700

    finis

---
+++ index.php
@@ -0,0 +1,69 @@
+<html>
+
+<head>
+    <title>SaCide PHP Batch 1 Day 04</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- HEADER -->
+        <div class="row">
+            <div class="col-12">
+                <h1>HEADER</h1>
+                <P>Your tag line here</P>
+            </div>
+        </div>
+        <!-- END HEADER -->
+
+
+        <!-- NAV -->
+        <!-- NAV -->
+        <div class="row bg-dark p-3">
+            <div class="col-12">
+
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <!-- END NAV -->
+        <!-- END NAV -->
+
+
+        <!-- CONTENT -->
+        <div class="row border-top">
+            <div class=" col-3">
+                MAIN CONTENT
+            </div>
+            <!-- END CONTENT -->
+
+
+            <!-- FOOTER -->
+            <div class="row  bg-dark p-3 text-light">
+                <div class="col-12">
+                    FOOTER
+                </div>
+            </div>
+            <!-- END FOOTER -->
+
+
+        </div>
+
+</body>
+
+</html>
---
+++ partials/header.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <title>SaCide PHP Batch 1 Day 04</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- HEADER -->
+        <div class="row">
+            <div class="col-12">
+                <h1>HEADER</h1>
+                <P>Your tag line here</P>
+            </div>
+        </div>
+        <!-- END HEADER -->
+
+
+        <!-- NAV -->
+        <!-- NAV -->
+        <div class="row bg-dark p-3">
+            <div class="col-12">
+
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <!-- END NAV -->
+        <!-- END NAV -->
